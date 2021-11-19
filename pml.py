d97bb1354adcf310d5245aa70aad2665825c8844
U-FCPSEDU\1593179 <1593179@3070N0098070052.fcps.edu>
2021-11-19 10:54:42 -0500

    Initial commit

---
+++ pml.py
@@ -0,0 +1,2 @@
+import sys,os.path as p,datetime as d
+l=lambda m:print((lambda l:f'{d.datetime.now().ctime()[11:19]} \033[90m{l[0]}\033[0m:\033[36m{l[1]}\033[96m {m}\033[0m')((lambda x:(p.relpath(x.f_code.co_filename),x.f_lineno))(sys._getframe(1))))
