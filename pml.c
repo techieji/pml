d000a80e7b06ac2d1d2daa3fd0664347e7b99d95
Pradhyum R <drpradhyum2016@outlook.com>
2022-02-05 18:24:29 +0000

    Added c logger

---
+++ pml.c
@@ -0,0 +1,2 @@
+#include <stdio.h>
+#define l(x) printf("%s:%d %s", __FILE__, __LINE__, #x)
--- pml.py
+++ pml.py
@@ -1,2 +1,2 @@
 import sys,os.path as p,datetime as d
-l=lambda m:print((lambda l:f'{d.datetime.now().ctime()[11:19]} \033[90m{l[0]}\033[0m:\033[36m{l[1]}\033[96m {m}\033[0m')((lambda x:(p.relpath(x.f_code.co_filename),x.f_lineno))(sys._getframe(1))))
+l=lambda m:print((lambda l:f'{d.datetime.now().ctime()[11:19]} \033[90m{l[0]}\033[0m:\033[36m{l[1]}\033[31m:{l[2]}\033[96m {m}\033[0m')((lambda x:(p.relpath(x.f_code.co_filename),x.f_lineno,x.f_code.co_name))(sys._getframe(1))))
